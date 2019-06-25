cde182eeddcd7fe02027fd7d3e1214aef6834d1c
sinkcup <sinkcup@gmail.com>
2019-06-25 23:42:10 +0800

    fix: phpunit files one by one output wrong clover.xml

--- phpunit.sh
+++ phpunit.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
-./vendor/bin/phpunit --version
-I=1
-FLAG=0
-for FILENAME in $(find tests/ -type f -name "*Test.php")
-do
-    printf '-'
-    I=$(($I + 1))
-    OUTPUT=$(./vendor/bin/phpunit $FILENAME)
-    if [ $? -ne 0 ]; then
-        FLAG=$(($FLAG + 1))
-        printf "\n$FILENAME\n"
-        echo "$OUTPUT"
-    fi
-done
+STARTED_AT=$(date +%s)
 
-printf "\n\n"
-if [ $FLAG -eq 0 ]; then
-    echo 'OK ('$I' files)'
-else
-    echo 'ERRORS!'
-    echo 'Files: '$I', Errors: '$FLAG'.'
+php artisan migrate:fresh
+php artisan migrate:refresh
+if [ $? -ne 0 ]; then
+    exit 1
 fi
+
+./vendor/bin/phpunit --stop-on-defect tests/
+FLAG=$?
+
+FINISHED_AT=$(date +%s)
+echo 'Time taken: '$(($FINISHED_AT - $STARTED_AT))' seconds'
 exit $FLAG
