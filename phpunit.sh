9e673e629012304803f58974935b551258572e45
sinkcup <sinkcup@gmail.com>
2019-06-26 12:55:06 +0800

    fix: sqlite error. sinkcup/laravel-make-auth-socialite#14

--- phpunit.sh
+++ phpunit.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 STARTED_AT=$(date +%s)
 
-php artisan migrate
+php artisan migrate:fresh
+php artisan migrate:refresh
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 ./vendor/bin/phpunit --stop-on-defect tests/
-FLAG=$?
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 FINISHED_AT=$(date +%s)
 echo 'Time taken: '$(($FINISHED_AT - $STARTED_AT))' seconds'
-exit $FLAG
