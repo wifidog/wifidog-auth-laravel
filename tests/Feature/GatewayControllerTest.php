1fc3dc16f3692cb1c13e420e3cc9b34147016078
sinkcup <sinkcup@gmail.com>
2019-06-27 12:44:08 +0800

    opt: improve test coverage

--- tests/Feature/GatewayControllerTest.php
+++ tests/Feature/GatewayControllerTest.php
@@ -34,4 +34,63 @@ class GatewayControllerTest extends TestCase
             $this->assertEquals($v, $gateway[$k]);
         }
     }
+
+    public function testPingWithBadParams()
+    {
+        $params = [
+            'gw_id' => $this->faker->word(),
+            'sys_uptime' => rand(1, 999),
+            'sys_memfree' => rand(1, 999),
+            'sys_load' => $this->faker->randomFloat(2, 0, 100),
+            'wifidog_uptime' => rand(1, 999),
+        ];
+        $key = array_rand($params);
+        $response = $this->call('GET', '/ping/', [$key => $params[$key]]);
+
+        $response->assertStatus(400);
+        $this->assertEquals('Error: params wrong', $response->getContent());
+    }
+
+    public function testPingFailedWhenNotAllowUnknownGateway()
+    {
+        config(['wifidog.allow_unknown_gateway' => false]);
+        $params = [
+            'gw_id' => $this->faker->word(),
+            'sys_uptime' => rand(1, 999),
+            'sys_memfree' => rand(1, 999),
+            'sys_load' => $this->faker->randomFloat(2, 0, 100),
+            'wifidog_uptime' => rand(1, 999),
+        ];
+        $response = $this->call('GET', '/ping/', $params);
+
+        $response->assertStatus(400);
+        $this->assertEquals('Error: not allow unknown gateway', $response->getContent());
+    }
+
+    public function testPingExistsGatewayWhenNotAllowUnknownGateway()
+    {
+        config(['wifidog.allow_unknown_gateway' => false]);
+        $gateway = factory(Gateway::class)->create();
+        $params = [
+            'gw_id' => $gateway->id,
+            'sys_uptime' => rand(1, 999),
+            'sys_memfree' => rand(1, 999),
+            'sys_load' => $this->faker->randomFloat(2, 0, 100),
+            'wifidog_uptime' => rand(1, 999),
+        ];
+        $response = $this->call('GET', '/ping/', $params);
+
+        $response->assertStatus(200);
+        $this->assertEquals('Pong', $response->getContent());
+
+        $gateways = Gateway::get();
+        $this->assertEquals(1, count($gateways));
+        $gateway = $gateways->first()->toArray();
+        $tmp = $params;
+        $tmp['id'] = $params['gw_id'];
+        unset($tmp['gw_id']);
+        foreach ($tmp as $k => $v) {
+            $this->assertEquals($v, $gateway[$k]);
+        }
+    }
 }
