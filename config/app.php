bbfacb3dd9d97c57cd1dee22b6411083fef244ee
sinkcup <sinkcup@gmail.com>
2019-07-05 22:12:58 +0800

    opt: remove Str because it cannot be mock. #33

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Eusonlito\LaravelMeta\MetaServiceProvider::class,
+        Webcraft\Random\RandomServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -215,6 +216,7 @@ return [
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
+        'Random' => Webcraft\Random\RandomFacade::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
