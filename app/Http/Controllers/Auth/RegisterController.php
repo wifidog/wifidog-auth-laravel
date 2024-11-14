7261ddb4d5f57d5e4cd04673acc9f60288fff691
Sink <sinkcup@gmail.com>
2024-11-14 20:36:34 +0800

    fix: #115 api_token is null after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'api_token' => bin2hex(random_bytes(16)),
         ]);
     }
 }
