322ccc1f0fb6464c2dc724686170dc759d827458
Sink <sinkcup@gmail.com>
2024-11-14 22:05:23 +0800

    fix: #116 not jump after register

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -12,4 +13,12 @@ class Controller extends BaseController
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+
+    public function __construct()
+    {
+        $request = request();
+        if (!empty($request->all())) {
+            session($request->all());
+        }
+    }
 }
