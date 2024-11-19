64bb85eeae023ad7033c46ebdc60cd99bb7adcbf
Sink <sinkcup@gmail.com>
2024-11-19 14:14:52 +0800

    style: migration as class

--- database/migrations/2017_04_29_093723_create_gateways_table.php
+++ database/migrations/2017_04_29_093723_create_gateways_table.php
@@ -4,14 +4,13 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGatewaysTable extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('gateways', function (Blueprint $table) {
             $table->string('id', 32)->primary('id');
@@ -30,8 +29,8 @@ class CreateGatewaysTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('gateways');
     }
-}
+};
